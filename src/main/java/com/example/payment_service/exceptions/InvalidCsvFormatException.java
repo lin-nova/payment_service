7721b035329c4c1463cae34c1e6af6993ea779c3
Alina Astapovich <alina.astapovich@electrolux.com>
2025-09-07 22:22:00 +0200

    Add exception handlers & security

--- src/main/java/com/example/payment_service/exceptions/InvalidCsvFormatException.java
+++
@@ -1,7 +0,0 @@
-package com.example.payment_service.exceptions;
-
-public class InvalidCsvFormatException extends RuntimeException {
-    public InvalidCsvFormatException(String message) {
-        super(message);
-    }
-}
